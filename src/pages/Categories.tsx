520980020587caf343a0064061668301aae17225
Gentrit Uka <gentrit-uka@live.com>
2022-08-03 15:39:48 +0200

    updated

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -20,11 +20,11 @@ export function Categories(){
         <div className="categories-container">
             <ul className="categories-container__list">
                 {categories.map(category => (
+                    <Link to={`/categories/${category.id}`}>
                     <a className="category-item">
-                        <Link to={`/categories/${category.id}`}>
-                            <h3>{category.name}</h3>
-                        </Link>
+                        {category.name}
                     </a>
+                    </Link>
                 ))}
             </ul>
         </div>
